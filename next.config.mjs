a00ab65a69da4fb5a875f148c55281e1b2b7130e
AdreesGujjar22 <madreesgujjar601@gmail.com>
2026-08-03 03:55:40 +0500

    fix version

--- next.config.mjs
+++ next.config.mjs
@@ -1,3 +1,7 @@
+import createNextIntlPlugin from "next-intl/plugin";
+
+const withNextIntl = createNextIntlPlugin("./i18n/request.ts");
+
 const nextConfig = {
   reactStrictMode: true,
   images: {
@@ -18,7 +22,6 @@ const nextConfig = {
       },
     ],
   },
-
 };
 
-export default nextConfig;
+export default withNextIntl(nextConfig);
